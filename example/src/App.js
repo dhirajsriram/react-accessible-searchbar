ec89834f65f022c38d419c966a04c3dc6da543b4
Dhiraj Sriram <Dhiraj86146@Mastek.Com>
2019-04-03 16:02:05 -0500

    First commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react'
-
-import ExampleComponent from 'react-searchbar'
-
+import Searchbar from 'react-searchbar'
+import styles from "./index.css"
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {search:""};
+    this.handleFieldChange = this.handleFieldChange.bind(this);
+  }
+
+  handleFieldChange(value) {
+    this.setState({search:value})
+  }
+  searchExample = (ev) => {
+    /* CUSTOM PROP FOR SEARCH
+    this.setState({ isLoaded: false })
+    fetch(searchapi + this.state.search).then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          this.setState({
+            isLoaded: true,
+            results: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        })
+    */
+  }
+
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <Searchbar onChange={this.handleFieldChange} search={(e)=>this.searchExample()} stylesheet={styles}/>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
 import styles from './styles.css'
+import { FaSearch } from 'react-icons/fa';
+import { FaTimes } from 'react-icons/fa';
+export default class Searchbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { search: "" };
+  }
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+  searchInput = (e) => {
+    const text = event.target.value;
+    this.setState({search : text})
+    this.props.onChange(text);
+  }
+
+  clearSearch = (e) => {
+    this.setState({ search: "", results: {}, isLoaded: true });
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
+      <form onSubmit={(e)=>{event.preventDefault();this.props.search()}}>
+      <div className={this.props.stylesheet["searchbar-collection-container"]}>
+        <div className={this.props.stylesheet["searchbar-container"]}>
+          <input type="text" id="searchbar-input" className={this.props.stylesheet["searchbar-input"]} onChange={(e) => this.searchInput()} name="st" value={this.state.search} maxLength="90" placeholder="Search" aria-label="Type to search. Navigate forward to hear suggestions" autoComplete="off" autoCorrect="off" autocapitolize="off" spellCheck="false" /></div>
+        <div className={this.props.stylesheet["searchbar-buttons-container"]}>
+          <button aria-label="Clear search text" className={this.props.stylesheet["buttons"]} onClick={(e) => this.clearSearch()}><FaTimes /></button>
+          <button type="submit" className={this.props.stylesheet["buttons"]} aria-label="submit search" title="Search"><FaSearch /></button></div>
+      </div></form>
     )
   }
 }
+
